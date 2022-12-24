8f8c4143d4f383f1c804dd99a0c95b02b28efd57
Natnael Mekonnen <naty@nany.zte.com.cn>
2022-12-24 10:31:10 -0500

    add pagination to getproduct request

--- src/controller/getProduct.js
+++ src/controller/getProduct.js
@@ -1,9 +1,11 @@
 const product=require('../models/Products')
 exports.getProduct=(req,res)=>{
-
+  const page=req.query.p || 0
+  const productperpage=9
     product.find()
        .sort({ _id: -1 })
-       .limit(8)
+       .skip(page*productperpage)
+       .limit(productperpage)
             .then((data)=>{
                 res.status(200).json({
                     message:data
