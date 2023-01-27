84d169ca41f5cad37578a2e4b2c302a82a69f299
Natnael Mekonnen <naty@nany.zte.com.cn>
2023-01-27 09:05:39 -0500

    remove the validations from admin routes

--- src/controller/vlaidate.js
+++ src/controller/vlaidate.js
@@ -71,7 +71,7 @@ exports.validateRequest=[
         }
         next()
     }
-    const storage = multer.diskStorage({
+    /*const storage = multer.diskStorage({
         destination: function (req, file, cb) {
           cb(null,path.join(path.dirname(__dirname),'uploads'))
         },
@@ -94,5 +94,5 @@ exports.validateRequest=[
 
 const upload1=multer({storage:storage1})
 exports.upload1=upload1
-   
+   */
    
